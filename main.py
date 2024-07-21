798586e5cc17e829100ff2d86c7882bd532ac278
Zeeshan Khalid <zeeshanthedev@gmail.com>
2024-07-21 22:28:38 +0500

    done

--- main.py
+++ main.py
@@ -14,24 +14,48 @@ with open('./sql/todos.sql', 'r') as file:
 cursor.executescript(sql_script)
 print('''TODO LIST
       
-      1. Add a todo (type add)
-      2. Delete a todo (type delete)''')
-options = input("your option: ")
-if options.lower() == "add":
-    while True:
-            title = input("Enter a todo (type 'exit' to end): ")
-            
-            if title.lower() == 'exit':
-                break
-            
-            # Insert the todo item into the table using a parameterized query
-            cursor.execute('''
-            INSERT INTO todos (title) VALUES (?)
-            ''', (title,))
+      1. Add a todo
+      2. Delete a todo
+      3. Show the todos
+      4. Exit''')
+while True:
+    options = input("your option: ")
+    if options == "4":
+         break
+    if options == "1":
+        while True:
+                title = input("Enter a todo (type 'exit' to end): ")
+                
+                if title.lower() == 'exit':
+                    break
+                
+                # Insert the todo item into the table using a parameterized query
+                cursor.execute('''
+                INSERT INTO todos (title) VALUES (?)
+                ''', (title,))
 
-elif options.lower() == "delete":
-     pass
-     
+    elif options == "2":
+        # id = input('enter the number of the todo to be removed :')
+        # cursor.execute('''
+        #         DELETE FROM todos where id = (?)
+        #         ''', (id,))
+        try:
+            id = int(input('Enter the ID of the todo to be removed: '))
+            cursor.execute('''
+            DELETE FROM todos WHERE id = ?
+            ''', (id,))
+            if cursor.rowcount == 0:
+                print("No todo found with that ID.")
+            else:
+                print("Todo removed.")
+        except ValueError:
+            print("Please enter a valid number.")
+    elif options == "3":
+        cursor.execute('SELECT * FROM todos')
+        todos = cursor.fetchall()
+        print(todos)  
+
+        
 
 conn.commit()
 conn.close()
